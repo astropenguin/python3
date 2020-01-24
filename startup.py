fa135939cab9224f2a05f0de3b4b862072c2136c
Akio Taniguchi <taniguchi@a.phys.nagoya-u.ac.jp>
2020-01-24 14:41:29 +0900

    Update startup script

---
+++ startup.py
@@ -0,0 +1,10 @@
+# standard library
+import os
+import sys
+from pathlib import Path
+
+# installed packages
+import numpy as np
+import pandas as pd
+import xarray as xr
+import matplotlib.pyplot as plt
